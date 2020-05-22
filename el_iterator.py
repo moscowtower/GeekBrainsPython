acb778beb665f5bc6b26861964ce498078e96cc6
Max <maxskavronsky@gmail.com>
2020-05-22 12:54:51 +0300

    Последние правки

--- el_iterator.py
+++ el_iterator.py
@@ -7,7 +7,7 @@ cycle_list = [i + randint(0, 10) for i in range(10)]
 i = 1
 print('Дарю тебе 10 случайных интов:')
 
-for el in cycle(cycle_list, 10):
+for el in cycle(cycle_list):
     if i > 10:
         break
     print(f'{i}: ', el)
--- salary_script.py
+++ salary_script.py
@@ -1,9 +1,12 @@
 from sys import argv
+def salary(hours, wage, reward):
+    salary = (int(hours) * int(wage)) + int(reward)
+    print(f'Расчет заработной платы исходя из: \n - ставки в {wage}$/час;\n - {hours} часов; \n - премии размером в {reward}$:')
+    print(f'Зарплата = {salary}$')
 
 try:
     script_name, hours, wage, reward = argv
-    salary = (int(hours)*int(wage)) + int(reward)
-    print(f'Расчет заработной платы исходя из: \n - ставки в {wage}$/час;\n - {hours} часов; \n - премии размером в {reward}$:')
-    print(f'Зарплата = {salary}$')
+    salary(hours, wage, reward)
+
 except ValueError or NameError:
     print('Требуется три позиционных параметра — кол-во рабочих часов, почасовая ставка и размер премии')
