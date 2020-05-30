1e8901eab106de6a0f05fe41e32886cf2145d239
Max <maxskavronsky@gmail.com>
2020-05-30 13:54:25 +0300

    добавлена обработка ValueError

--- Road.py
+++ Road.py
@@ -3,7 +3,11 @@ class Road:
         self._length = length
         self._width = width
     def calc_asphalt_mass(self, thickness):
-        print('Требуемая масса асфальта:',(self._length * self._width * 25 * int(thickness))//1000, 'тонн')
+        try:
+            result = self._length * self._width * 25 * int(thickness) // 1000
+            print('Требуемая масса асфальта:', result, 'тонн')
+        except ValueError as e:
+            print(e)
 
 Doroga = Road(20, 5000)
 Doroga.calc_asphalt_mass(input('Введите желаемую толщину асфальта(см): '))
